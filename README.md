ed1d9d9907d4f4183fb3e5e4fd8cfcf3c1b8d972
Beckie Yanovsky <beckie@mixpanel.com>
2014-11-13 16:41:47 -0800

    Updated ReadME and took Mixpanel Class definition out of TrackEvent.py

--- README.md
+++ README.md
@@ -3,7 +3,23 @@ mixpanel-fake-data-gen
 
 Mixpanel Fake Data Generator
 
-Flexible platform to make fake data in a Mixpanel project. Populate
-- A funnel
-- Y
-- Z
+Flexible platform to make fake data in a Mixpanel project.
+Version 1 populates a simple 3- step funnel.
+
+Flexibility comes in changing params in the 'MakeConversionFunnel.py' script where you can set:
+
+(Decimal) conversion rates between steps:
+PERCENT_STEP1_STEP2 = .85
+PERCENT_STEP2_STEP3 = .6
+
+(String) event names:
+EVENT_NAME_1 = "Landing Page Loaded"
+EVENT_NAME_2 = "Signup Page Loaded"
+EVENT_NAME_3 = "Signup Complete"
+
+(Integer) number of initial users you would like to be part of this funnel:
+INITIAL_USERS = 100
+
+More flexibility to come!
+
+Mixpanel class definition found in beckiesmixpanel.py
--- TrackEvent.py
+++ TrackEvent.py
@@ -1,62 +1,16 @@
-# Beckie's first Python script that sends an event to her test project
+# Beckie's first Python script that sends an event to her test project using the Mixpanel class created in beckiesmixpanel.py
+
 
 #set of funnels that convert 
 #retention rates
 #^both of the above varying for different TYPES of users (type is as you define)
 
 #import libraries
-import base64
-import json
-import time
-from urllib2 import urlopen
-import urllib2
-import urllib
+import beckiesmixpanel
 import credentials
 
-ENDPOINT = 'https://api.mixpanel.com/track'
-
-
-class Mixpanel(object):
-	def __init__(self, api_key, api_secret, token):
-		self.api_key = api_key
-		self.api_secret = api_secret
-		self.token = token
-
-	def track(self, distinct_id, event_name, added_properties={}):
-		"""
-		Pass this method a Mixpanel object, distinct_id, event_name, and hash table of properties 
-		and it will track that event in the mixpanel project with same credentials as Mixpanel object
-		that called it.
-		"""
-
-		properties = {
-			'distinct_id' : distinct_id,
-			'time' : int(time.time()),
-			'token' : mp.token
-		}
-		properties.update(added_properties)
-		event = {
-			'event' : event_name,
-			'properties' : properties
-		}
-		event_json = json.dumps(event, separators=(',', ':'))
-		data = {
-			'data':base64.b64encode(event_json),
-			'verbose':1,
-			'ip':0,
-			'api_key': mp.api_key
-		}
-
-
-		encoded_data = urllib.urlencode(data)
-		print encoded_data
-		request = urllib2.Request(ENDPOINT, encoded_data)
-
-		response = urllib2.urlopen(request).read()
-		print response
-
 if __name__ == '__main__':
-	mp = Mixpanel(
+	mixpanel = beckiesmixpanel.Mixpanel(
 		api_key = credentials.api_key,
 		api_secret = credentials.api_secret,
 		token = credentials.token
@@ -66,4 +20,4 @@ if __name__ == '__main__':
 		event = "event" + str(user)
 		for letter in "Test":
 			properties.update({"random_property_"+letter : letter})
-			mp.track(user, event, properties)
+			mixpanel.track(user, event, properties)
