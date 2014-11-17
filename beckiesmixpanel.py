5b512565c1b31205879eb7a23bfd69967daeed83
Beckie Yanovsky <beckie@mixpanel.com>
2014-11-17 11:10:30 -0800

    added optional time argument to the track method

--- beckiesmixpanel.py
+++ beckiesmixpanel.py
@@ -3,6 +3,7 @@
 
 import base64
 import credentials
+import datetime
 import json
 import random
 import time
@@ -10,7 +11,7 @@ from urllib2 import urlopen
 import urllib2
 import urllib
 
-ENDPOINT = 'https://api.mixpanel.com/track'
+ENDPOINT = 'https://api.mixpanel.com/import'
 
 
 class Mixpanel(object):
@@ -18,6 +19,13 @@ class Mixpanel(object):
 		self.api_key = api_key
 		self.api_secret = api_secret
 		self.token = token
+
+	#time variables
+	now = int(time.time())
+	one_day = datetime.timedelta(days=1).total_seconds()
+	seven_days = datetime.timedelta(days=7).total_seconds()
+	one_month = datetime.timedelta(weeks=4).total_seconds()
+	six_month = datetime.timedelta(weeks=24).total_seconds()
 
 	#This method randomly assigns a gender value 
 	def assign_gender(self):
@@ -29,7 +37,7 @@ class Mixpanel(object):
 		os = random.choice(['Linux', 'Unix', 'Windows', 'OSX', 'iOS','Android'])
 		return os
 
-	def track(self, distinct_id, event_name, added_properties={}):
+	def track(self, distinct_id, event_name, added_properties={}, time = int(time.time())):
 		"""
 		Pass this method a Mixpanel object, distinct_id, event_name, and hash table of properties 
 		and it will track that event in the mixpanel project with same credentials as Mixpanel object
@@ -37,7 +45,7 @@ class Mixpanel(object):
 		"""
 		properties = {
 			'distinct_id' : distinct_id,
-			'time' : int(time.time()),
+			'time' : time,
 			'token' : self.token
 		}
 		properties.update(added_properties)
@@ -57,6 +65,7 @@ class Mixpanel(object):
 		encoded_data = urllib.urlencode(data)
 		#print encoded_data
 		request = urllib2.Request(ENDPOINT, encoded_data)
+		print request
 
 		response = urllib2.urlopen(request).read()
-		#print response
+		print response
