16ca28284effc6db3ee46edda78779e009f526fc
Beckie Yanovsky <beckie@mixpanel.com>
2014-11-17 11:19:48 -0800

    added time period to make_conversion_funnel

--- MakeConversionFunnel.py
+++ MakeConversionFunnel.py
@@ -13,7 +13,12 @@
 #import libraries
 import beckiesmixpanel
 import credentials
+import datetime
+import json
 import random
+import time
+from time import mktime
+
 
 
 # Please enter a decimal for the percent conversion you'd like for each step. For example, .35 would indicate a 35% conversion rate
@@ -27,14 +32,20 @@ EVENT_NAME_2 = "Signup Page Loaded"
 EVENT_NAME_3 = "Signup Complete"
 
 #Please enter the number of initial users you would like to be part of this funnel. 
-INITIAL_USERS = 100
+INITIAL_USERS = 1000
 
+#UTC time
+T = datetime.datetime(1970, 1, 1)
 
 
 
 #This method takes in a distinct_id, a dictionary of {"event_name", {"property_name":property value}}, 
 # a list of events, and a list of conversion floats and makes a funnel 
-def move_user_through_conversion_funnel(self, distinct_id, events_dict, events_list, conversions):
+def move_user_through_conversion_funnel(self, distinct_id, events_dict, events_list, conversions, time_period):
+	#check if valid value for time_period; else sets to minutes
+	valid_times = ['minutes', 'days', 'weeks']
+	if time_period not in valid_times:
+		time_period = 'minutes'
 	#check if number of conversion percentages is one less than number of events
 	while (len(conversions)+1) != len(events_list):
 		#if not enough conversion percentages given, append 0.0 to the list until there are enough
@@ -44,13 +55,19 @@ def move_user_through_conversion_funnel(self, distinct_id, events_dict, events_l
 		elif ((len(conversions) >= (len(events_list)))):
 			conversions.pop()
 	#for every user, tracks the first event in the funnel
-	self.track(distinct_id, events_list[0], events_dict.get(events_list[0]))
+	start_time = int((datetime.datetime.utcfromtimestamp(self.now - random.randint(0, self.six_month))-T).total_seconds())
+	#print start_time
+	self.track(distinct_id, events_list[0], events_dict.get(events_list[0]), start_time)
+	print distinct_id
+	event_time = start_time
 	iterator = 0
 	while iterator <= (len(conversions)-1):
 		r = random.random()
-		print r
 		if r < conversions[iterator]:
-			self.track(distinct_id, events_list[(iterator+1)], events_dict.get(events_list[(iterator+1)]))
+			interval_type = time_period
+			interval_num = random.randint(1, 5)
+			event_time += (datetime.timedelta(**{interval_type: interval_num})).total_seconds()
+			self.track(distinct_id, events_list[(iterator+1)], events_dict.get(events_list[(iterator+1)]), event_time)
 			print events_list[(iterator+1)]
 		iterator += 1
 
@@ -75,8 +92,5 @@ if __name__ == '__main__':
 		for event in events:
 			event_prop_dict[event]=super_properties
 		#move a user through the funnel based on conversion rates provided above
-		move_user_through_conversion_funnel(mp, distinct_id, event_prop_dict, events, conversions)
+		move_user_through_conversion_funnel(mp, distinct_id, event_prop_dict, events, conversions, "minutes")
 		distinct_id += 1
-
-
-
